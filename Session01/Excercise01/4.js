e7034c90f1ae7405eab6cf3ed15b785bef06520e
Trí <asylumcrow.code@gmail.com>
2021-11-19 15:09:09 +0700

    Refactor

--- Session01/Excercise01/4.js
+++
@@ -1,9 +0,0 @@
-let input = 'image.png'
-
-let fileType = function (input) {
-    let dotPos = input.lastIndexOf('.');
-    if (typeof input === 'string') console.log('output: ' + input.substring(dotPos + 1, input.length));
-}
-
-console.log('input: ' + input);
-fileType(input);
--- Session01/Excercise02/3.js
+++
@@ -1,9 +0,0 @@
-let arr = [1, 2, 3, 4, 5, 6];
-
-let randomElement = function (arr) {
-    let randomIndex = Math.floor(Math.random() * (arr.length - 1));
-    return arr[randomIndex];
-}
-
-console.log('input: ' + arr);
-console.log('output: ' + randomElement(arr));
